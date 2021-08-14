2e487dbd82a7a2d1e44a3e8fb2a5aa631a379209
Yaroslav Kulyk <kulyk.yaroslav@gmail.com>
2021-08-14 15:32:56 +0300

    Сделал домашнее задание

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const debounce = require('lodash.debounce');
+
 import './sass/main.scss';
 import API from './js/fetchCountries';
 import countriesListTpl from './templates/countries-list.hbs';
@@ -5,7 +7,6 @@ import countryCard from './templates/country-card.hbs';
 
 const resultContainerRef = document.querySelector('#country-card');
 const inputRef = document.querySelector('#q-string');
-const searchBtnRef = document.querySelector('#search-btn');
 
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/desktop/dist/PNotifyDesktop';
@@ -15,31 +16,48 @@ import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/desktop/dist/PNotifyDesktop';
 import '@pnotify/core/dist/BrightTheme.css';
 
-searchBtnRef.addEventListener('click', onClick);
-window.addEventListener('keydown', e => {
-  if (e.code === 'Enter') onClick();
-});
+inputRef.addEventListener('input', debounce(onIputFinish, 500));
 
-function onClick() {
-  API.fetchCountries(inputRef.value)
-    .then(handleNotFound)
-    .then(response => response.json())
-    .then(countriesArray => {
-      if (countriesArray.length > 10) {
-        error({
-          title: `Too many matches found.`,
-          text: `Please enter a more specific query`,
-          styling: 'brighttheme',
-          delay: 3000,
-        });
-        return;
-      }
-      renderCountryCard(countriesArray);
+function onIputFinish() {
+  if (inputRef.value === '') {
+    return;
+  }
+
+  API.fetchCountries(inputRef.value).then(handleNotFound).then(checkNumberOfCountries);
+}
+
+function handleNotFound(response) {
+  if (response.status === 404) {
+    error({
+      title: `No mathces found`,
+      text: `Make sure you're looking a country`,
+      styling: 'brighttheme',
+      delay: 2500,
     });
+    return;
+  }
+
+  return response.json();
+}
+
+function checkNumberOfCountries(countriesArray) {
+  if (!countriesArray) {
+    return;
+  }
+
+  if (countriesArray.length > 10) {
+    error({
+      title: `Too many matches found.`,
+      text: `Please enter a more specific query`,
+      styling: 'brighttheme',
+      delay: 2500,
+    });
+    return;
+  }
+  renderCountryCard(countriesArray);
 }
 
 function renderCountryCard(country) {
-  console.log(country);
   if (country.length > 1) {
     const markup = countriesListTpl(country);
     resultContainerRef.innerHTML = markup;
@@ -47,17 +65,4 @@ function renderCountryCard(country) {
   }
 
   resultContainerRef.innerHTML = countryCard(country);
-}
-
-function handleNotFound(response) {
-  if (response.status === 404) {
-    error({
-      title: `No mathces found`,
-      text: `Make sure you enter a alphabetic symbols`,
-      styling: 'brighttheme',
-      delay: 3000,
-    });
-    return;
-  }
-  return response;
 }
